0dfc674f5b9440c6390568fc770d20fe63aa5d0a
Admin <orenakresh@gmail.com>
2026-01-27 19:05:06 -0600

    jack poopy face

--- robot/fuel_sim.py
+++ robot/fuel_sim.py
@@ -74,46 +74,50 @@ from wpilibextra.coroutine import CoroutineCommand
 from wpilib import SmartDashboard
 
 class FuelSim:
-    period = 0.02
-    subticks = 5
-    gravity = Translation3d(0, 0, -9.81) # m/s^2
-    
-    #coefficients of restitution
-    field_cor = math.sqrt(22 / 51.5)
-    fuel_cor = 0.5
-    net_cor = 0.2
-    robot_cor = 0.1
+    def __init__(self):
+        self.period = 0.02
+        self.subticks = 5
+        self.gravity = Translation3d(0, 0, -9.81) # m/s^2
+        
+        #coefficients of restitution
+        self.field_cor = math.sqrt(22 / 51.5)
+        self.fuel_cor = 0.5
+        self.net_cor = 0.2
+        self.robot_cor = 0.1
 
-    fuel_radius = 0.075
-    field_length = 16.51
-    field_width = 8.04
-    friction = 0.1
+        self.fuel_radius = 0.075
+        self.field_length = 16.51
+        self.field_width = 8.04
+        self.friction = 0.1
 
-    instance = None
+        self.instance = None
 
-    field_xz_line_starts = [
-        Translation3d(0,0,0),
-        Translation3d(3.96, 1.57, 0),
-        Translation3d(3.96, field_width / 2 + 0.60, 0),
-        Translation3d(4.61, 1.57, 0.165),
-        Translation3d(4.61, field_width / 2 + 0.60, 0.165),
-        Translation3d(field_length -5.18, 1.57, 0),
-        Translation3d(field_length - 5.18, field_width / 2 + 0.60, 0),
-        Translation3d(field_length - 4.61, 1.57, 0.165),
-        Translation3d(field_length - 4.61, field_width / 2 + 0.60, 0.165)
-    ]
+        self.field_xz_line_starts = [
+            Translation3d(0,0,0),
+            Translation3d(3.96, 1.57, 0),
+            Translation3d(3.96, self.field_width / 2 + 0.60, 0),
+            Translation3d(4.61, 1.57, 0.165),
+            Translation3d(4.61, self.field_width / 2 + 0.60, 0.165),
+            Translation3d(self.field_length -5.18, 1.57, 0),
+            Translation3d(self.field_length - 5.18, self.field_width / 2 + 0.60, 0),
+            Translation3d(self.field_length - 4.61, 1.57, 0.165),
+            Translation3d(self.field_length - 4.61, self.field_width / 2 + 0.60, 0.165)
+        ]
 
-    field_xz_line_ends = [
-        Translation3d(field_length, field_width, 0),
-        Translation3d(4.61, field_width / 2 - 0.60, 0.165),
-        Translation3d(4.61, field_width - 1.57, 0.165),
-        Translation3d(5.18, field_width / 2 - 0.60, 0),
-        Translation3d(5.18, field_width - 1.57, 0),
-        Translation3d(field_length - 4.61, field_width / 2 - 0.60, 0.165),
-        Translation3d(field_length - 4.61, field_width - 1.57, 0.165),
-        Translation3d(field_length - 3.96, field_width / 2 - 0.60, 0),
-        Translation3d(field_length - 3.96, field_width - 1.57, 0)
-    ]
+        self.field_xz_line_ends = [
+            Translation3d(self.field_length, self.field_width, 0),
+            Translation3d(4.61, self.field_width / 2 - 0.60, 0.165),
+            Translation3d(4.61, self.field_width - 1.57, 0.165),
+            Translation3d(5.18, self.field_width / 2 - 0.60, 0),
+            Translation3d(5.18, self.field_width - 1.57, 0),
+            Translation3d(self.field_length - 4.61, self.field_width / 2 - 0.60, 0.165),
+            Translation3d(self.field_length - 4.61, self.field_width - 1.57, 0.165),
+            Translation3d(self.field_length - 3.96, self.field_width / 2 - 0.60, 0),
+            Translation3d(self.field_length - 3.96, self.field_width - 1.57, 0)
+        ]
+
+        self.blue_hub = self.Hub(self, Translation2d(4.61, self.field_width / 2), Translation3d(5.3, self.field_width / 2, 0.89), 1)
+        self.red_hub = self.Hub(self, Translation2d(self.field_length - 4.61, self.field_width / 2), Translation3d(self.field_length - 5.3, self.field_width / 2, 0.89), -1)
 
     class Fuel():
         def __init__(self, sim : "FuelSim", pos : Translation3d, vel : Translation3d):
@@ -175,8 +179,50 @@ class FuelSim:
                 self.vel += Translation3d(0, -(1 + self.sim.field_cor) * self.vel.Y(), 0)
 
             # hubs
-           #2 handleHubCollisions
-    
+            self.handleHubCollisions(self.sim.blue_hub)
+            self.handleHubCollisions(self.sim.red_hub)
+        
+        def handleHubCollisions(self, hub : "FuelSim.Hub"):
+            hub.handleHubInteraction(self)
+            collision = hub.fuelCollideSide(self)
+            if collision.X() != 0:
+                self.pos += Translation3d(collision)
+                self.vel += Translation3d(-(1 + self.sim.field_cor) * self.vel.X(), 0, 0)
+            elif collision.Y() != 0:
+                self.pos += Translation3d(collision)
+                self.vel += Translation3d(0, -(1 + self.sim.field_cor) * self.vel.Y(), 0)
+
+            netCollision = hub.fuelHitNet(self)
+            if netCollision != 0:
+                self.pos += Translation3d(netCollision, 0, 0)
+                self.vel = Translation3d(-self.vel.X() * self.sim.net_cor, self.vel.Y() * self.sim.net_cor, self.vel.Z())
+
+        def addImpulse(self, impulse : Translation3d):
+            self.vel += impulse
+
+    def handleFuelCollision(self, a : Fuel, b : Fuel):
+        normal = a.pos - b.pos
+        distance = normal.norm()
+        if distance == 0:
+            normal = Translation3d(1, 0, 0)
+            distance = 1
+        normal = normal / distance
+        impulse = 0.5 * (1 + self.fuel_cor) * (b.vel - a.vel).dot(normal)
+        intersection = self.fuel_radius * 2 - distance
+        a.pos += normal * (intersection / 2)
+        b.pos -= normal * (intersection / 2)
+
+        a.addImpulse(normal * impulse)
+        b.addImpulse(normal * -impulse)
+
+    def handleFuelCollisions(self, fuels : list[Fuel]):
+        for i in range(len(fuels) - 1):
+            for j in range(i + 1, len(fuels)):
+                if fuels[i].pos.distance(fuels[j].pos) < self.fuel_radius * 2
+
+
+
+
     class Hub():
         def __init__(self, sim : "FuelSim", center : Translation2d, exit : Translation3d, exitVelXMult : int):
             # constants
@@ -231,6 +277,39 @@ class FuelSim:
 
             if distance_to_left > 0 or distance_to_right > 0 or distance_to_top > 0 or distance_to_bottom > 0:
                 return Translation2d() # not inside hub
+            
+            if (fuel.pos.X() < self.center.X() - self.side / 2
+                or (
+                    distance_to_left >= distance_to_right
+                and distance_to_left >= distance_to_top
+                and distance_to_left >= distance_to_bottom)):
+                return Translation2d(distance_to_left, 0)
+            elif (fuel.pos.X() >= self.center.X() + self.side / 2
+                or (
+                    distance_to_right >= distance_to_left
+                and distance_to_right >= distance_to_top
+                and distance_to_right >= distance_to_bottom)):
+                return Translation2d(-distance_to_right, 0)
+            elif (fuel.pos.Y() > self.center.Y() + self.side / 2
+                or (
+                    distance_to_top >= distance_to_left
+                and distance_to_top >= distance_to_right
+                and distance_to_top >= distance_to_bottom)):
+                return Translation2d(0, -distance_to_top)
+            else:
+                return Translation2d(0, distance_to_bottom)
+            
+        def fuelHitNet(self, fuel : "FuelSim.Fuel"):
+            if fuel.pos.Z() > self.net_height_max or fuel.pos.Z() < self.net_height_min:
+                return 0
+            
+            if fuel.pos.Y() > self.center.Y() + self.net_width / 2 or fuel.pos.Y() < self.center.Y() - self.net_width / 2:
+                return 0
+            
+            if fuel.pos.X() > self.center.X() + self.net_offset * self.exitVelXMult:
+                return max(0, self.center.X() + self.net_offset * self.exitVelXMult - (fuel.pos.X() - self.sim.fuel_radius))
+            else:
+                return min(0, self.center.X() + self.net_offset * self.exitVelXMult - (fuel.pos.X() + self.sim.fuel_radius))
             
             
             
